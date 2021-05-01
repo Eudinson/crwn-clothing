60cf8b7894dbf18d4cc8b11b9aae416aee1fa957
Eudinson <eudinson_uy29@yahoo.com>
2021-05-01 16:53:17 +0400

    set up routing on menu items for navigation / withRouter HOC or higher order component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 
 function App() {
+
+  const Hats = () => {
+    return(
+      <h1>Hats Page</h1>
+    )
+  }
+
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/hats' component={Hats}/>
+      </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
